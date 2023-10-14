72fd699a042d2eb293cf60410ee8a9382c479926
Henrique Pureza Queiroz <henrique.pureza.queiroz@gmail.com>
2023-10-14 18:17:22 -0300

    Rollback

--- Nutri.py
+++ Nutri.py
@@ -13,8 +13,6 @@
 #                                           Bibliotecas                                            #
 #--------------------------------------------------------------------------------------------------#
 
-# Teste
-
 import sqlite3
 
 from flask                   import Flask, render_template, request, redirect
